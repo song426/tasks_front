1685feddb4927ad5d5661363888b8b84c9469a59
song426 <seolsongie@gmail.com>
2024-11-06 16:32:55 +0900

    first commit

---
+++ src/Proceeding/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const index = () => {
+  return (
+    <div className="page_section">
+      <Navbar />
+    </div>
+  );
+};
+
+export default index;
