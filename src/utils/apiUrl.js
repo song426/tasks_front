bae750576a48314f7d0497caa1c6d72230840efd
song426 <seolsongie@gmail.com>
2024-11-18 15:44:54 +0900

    edit endpoint

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,5 +1,5 @@
-const rootPath = "http://localhost:8000";
-// const rootPath = "http://18.215.154.81";
+// const rootPath = "http://localhost:8000";
+const rootPath = "https://back.aiccsong.site";
 
 const GET_TASKS_API_URL = `${rootPath}/getTasks`;
 const DELETE_TASK_API_URL = `${rootPath}/deleteTask`;
